2c2d3b0b2f39d9020e29ca4853493b8aaf8caedd
Patrik Gustafsson <patrik.gustafsson@proagile.se>
2022-01-20 13:13:56 +0100

    After Kata 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# FizzBuzz2
+
+A description of this package.
---
+++ Sources/FizzBuzz2/FizzBuzz2.swift
@@ -0,0 +1,23 @@
+public struct FizzBuzz2 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+
+    func lineNumber(number: Int) -> String {
+        return String(number)
+    }
+}
+
+
+/*
+ Repeate
+ Navigator/typist(driver)
+
+ Overiview
+ Red
+ Green
+ Refactor
+
+
+ */
